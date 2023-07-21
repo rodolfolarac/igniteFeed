f9a812212b5d4140738a09b342ec7d71b2622095
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-21 07:36:21 -0300

    estilizações globais realizadas, component header finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import "../src/components/global.css"
+import styles from "../App.module.css"
+
 
 export function App() {
   return (
-    <>
-      <Post
-        author="Rodolfo Lara"
-        content="testetetststetstsgststgsajhghjagshjagsah"
-      />
-      <Post
-        author="Jean Carlos"
-        content="testetetststetstsssssssgststgsajhghjagshjagsah"
-      />
-    </>
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+
+      </div>
+    </div>
   )
 }
 
