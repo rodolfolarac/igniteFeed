b95cfb5300b9eb026c171bb1973d73f022361a78
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-20 22:16:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post
+        author="Rodolfo Lara"
+        content="testetetststetstsgststgsajhghjagshjagsah"
+      />
+      <Post
+        author="Jean Carlos"
+        content="testetetststetstsssssssgststgsajhghjagshjagsah"
+      />
+    </>
+  )
+}
+
