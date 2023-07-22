29d5ff233b9bdb11159339e10f0a45c15fbb1829
Rodolfo <rodolfo.cassaro@auaha.net>
2023-07-22 10:47:05 -0300

    component sidebar finalizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react"
+
 import styles from "./Sidebar.module.css"
 
 export function Sidebar() {
@@ -8,12 +10,16 @@ export function Sidebar() {
             src="https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://www.github.com/rodolfolarac.png" alt="Profile Image" />
+
                 <strong>Rodolfo Lara</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil</a>
             </footer>
         </aside>
     );
